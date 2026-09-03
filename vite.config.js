6558e57678ab6b58bd218060863849e44b33baae
Marc <marcuscolin.tolod@gmail.com>
2026-09-03 22:47:08 +0800

    more chanfes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/feverglass-website/',
+  base: '/feverglass-website',
   server: {
     proxy: {
       '/api': {
