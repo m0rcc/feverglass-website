04240dd45e646cca61000df23049864711706935
Marc <marcuscolin.tolod@gmail.com>
2026-09-03 23:00:31 +0800

    Made more changes so HashRouter is available, again...

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import BleedForIt from './pages/albums/bleedforit/BleedForIt'
 
 const App = () => {
   return (
-    <HashRouter basename='/feverglass-website'>
+    <HashRouter>
       <Routes>
         <Route path='' element={<Home />}> </Route>
         <Route path='home' element={<Home />}>  </Route>
